a431582a6b91f575dafb3651a35a96213758c951
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2019-11-25 12:14:15 +0530

    Added Screenshot & Reporting Till Pt 10

--- conf/conf.js
+++ conf/conf.js
@@ -1,4 +1,11 @@
 // An example configuration file.
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html'
+});
+
 exports.config = {
   directConnect: true,
 
@@ -17,5 +24,24 @@ exports.config = {
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+  
+  // Setup the report before any tests start
+  beforeLaunch: function() {
+    return new Promise(function(resolve){
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(reporter);
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
   }
 };
