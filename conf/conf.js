81fa7c3e7bff2e10cd8f74f5cc91ecffc9baad34
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2019-11-27 12:04:45 +0530

    HTML Reporter with Screenshot Till Pt 12

--- conf/conf.js
+++ conf/conf.js
@@ -47,6 +47,36 @@ exports.config = {
         done();
       })
     });
+    // HTML reporter 2
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: './',
+        filePrefix: 'xmlresults'
+    }));
+
+    // For screenshot for HTML reporter
+    var fs = require('fs-extra');
+ 
+    fs.emptyDir('screenshots/', function(err) {
+      console.log(err);
+    });
+    
+    jasmine.getEnv().addReporter({
+        specDone: function(result) {
+            if (result.status == 'failed') {
+                browser.getCapabilities().then(function(caps) {
+                    var browserName = caps.get('browserName');
+    
+                    browser.takeScreenshot().then(function(png) {
+                        var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName + '.png');
+                        stream.write(new Buffer(png, 'base64'));
+                        stream.end();
+                    });
+                });
+            }
+        }
+    });
   },
 
   // Close the report after all tests finish
@@ -54,5 +84,32 @@ exports.config = {
     return new Promise(function(resolve){
       reporter.afterLaunch(resolve.bind(this, exitCode));
     });
+  },
+
+  //HTMLReport called once tests are finished
+  onComplete: function() {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('protractor-html-reporter-2');
+
+      testConfig = {
+          reportTitle: 'Protractor Test Execution Report',
+          outputPath: './',
+          outputFilename: 'ProtractorTestReport',
+          screenshotPath: './screenshots',
+          testBrowser: browserName,
+          browserVersion: browserVersion,
+          modifiedSuiteName: false,
+          screenshotsOnlyOnFailure: true,
+          testPlatform: platform
+      };
+      new HTMLReport().from('xmlresults.xml', testConfig);
+  });
   }
 };
